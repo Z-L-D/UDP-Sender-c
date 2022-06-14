8fdf7313a630916979af0bd046cf5a944c18a566
Z-L-D <zacldoyle@gmail.com>
2022-06-14 17:23:02 -0500

    -

--- program.c
+++ program.c
@@ -3,6 +3,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#pragma comment(lib,"Ws2_32.lib")
+
 int main(void)
 {
     // Initialize Winsock 
@@ -10,7 +12,7 @@ int main(void)
     WSAStartup(MAKEWORD(2, 2), &wsaData);
 
     const char* command = "this is a test";
-    const char address[] = "127.0.0.1";
+    PCWSTR address = L"127.0.0.1";
     short port = 5500;    
 
     SOCKET socket_object = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
@@ -18,11 +20,10 @@ int main(void)
     struct sockaddr_in server_object;
     server_object.sin_family = AF_INET;
     server_object.sin_port = htons(port);
-    server_object.sin_addr.s_addr = inet_addr(address);
+    //server_object.sin_addr.s_addr = inet_addr(address);
+    InetPton(AF_INET, address, &server_object.sin_addr.s_addr);
 
     sendto(socket_object, command, (int)strlen(command), 0, (SOCKADDR*)&server_object, sizeof(server_object));
 
     closesocket(socket_object);
-
-    return 0;
 }
